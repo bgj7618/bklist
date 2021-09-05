97e28e5f686ea8cda38edb68b0e843402247f233
smion <smion@Byun-Home>
2021-09-06 00:55:48 +0900

    9.5 update
    # WARNING: head commit changed in the meantime
    
    8.31 수정
    
    Change-Id: I657beb0866f2741318848009beb694a20a17097b

--- bklist/src/main/java/com/buklist/mapper/BoardMapper.java
+++ bklist/src/main/java/com/buklist/mapper/BoardMapper.java
@@ -7,6 +7,12 @@ import org.apache.ibatis.annotations.Select;
 import com.buklist.domain.BoardVO;
 
 public interface BoardMapper {
-	@Select("select * from tbl_board where bno > 0")
-	public ArrayList<BoardVO> getList();
+	// @Select("select * from tbl_board where bno > 0")
+	public ArrayList<BoardVO> getList(); // 글목록
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board); //글 쓰기
+	
+	public BoardVO read(int bno); // 상세페이지
 }
